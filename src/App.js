2a86bfb260438ea0fb8153930b83284c501b0dd6
atere21 <you@tosinateret@gmail.com>
2023-09-10 06:14:57 -0700

    stageone

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProfileInfo from './components/ProfileInfo';
+
+function App() {
+  return (
+    <div className="container mx-auto mt-8">
+      <ProfileInfo />
+    </div>
+  );
+}
+
+export default App;
